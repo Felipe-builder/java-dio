99a644deb31f47ff8b578005c36d3dab9e076fb9
Felipe-builder <felipesvascon@gmail.com>
2022-01-20 18:33:31 -0300

    Intrudocao Aula dio Ecossistema JAVA

---
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.ecossistemaEDocumentacaoJava;
+
+import one.digitalinnovation.ecossistemaEDocumentacaoJava.base.Order;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        Order order = new Order("e1020eok");
+        System.out.println(order);
+    }
+}
