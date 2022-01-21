e7c5e8d43bfa5c6cd366cbb421c194c75da87543
Felipe-builder <felipesvascon@gmail.com>
2022-01-21 12:43:31 -0300

    aulas de ecossistema de java

--- src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/MyFirstProgram.java
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/MyFirstProgram.java
@@ -4,7 +4,10 @@ import one.digitalinnovation.ecossistemaEDocumentacaoJava.base.Order;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        Order order = new Order("e1020eok");
+        Order order = new Order("e1020eok", 250.42, new String[]{"arroz", "feijão", "carne"});
         System.out.println(order);
+        System.out.println(order.caculatedIf());
+        System.out.println(order.caculatedSwitch());
+        order.showList();
     }
 }
--- src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/base/Order.java
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/base/Order.java
@@ -1,10 +1,82 @@
 package one.digitalinnovation.ecossistemaEDocumentacaoJava.base;
 
-public class Order {
-    private final String code;
+/**
+ * @author felipe
+ * @version 1.0.0
+ * @see java.math.BigDecimal
+ * @since 1.0.0
+ */
 
-    public Order(String code) {
+public class Order {
+
+    // codigo
+    private final String code;
+    private final double totalValue;
+
+    private String[] list;
+
+    /**
+     * construtor da class
+     * @param code  coidigo do pedido
+     * @param totalValue valor todal do pedido
+     * @param list lista do pedido
+     */
+    public Order(String code, double totalValue, String[] list) {
         this.code = code;
+        this.totalValue = totalValue;
+        this.list = list;
+    }
+
+    /**
+     *  Elee calcula o valor total de acordo como pedido
+     * @return retorna o valor do pedido
+     * @throws RuntimeException
+     */
+    public double caculatedIf() throws RuntimeException {
+        if (this.totalValue < 0) {
+            throw new RuntimeException("O valor não pode ser negativo");
+        }
+        if (totalValue > 100) {
+            return totalValue * 0.99;
+        } else {
+            return totalValue;
+        }
+    }
+
+    public double caculatedSwitch(){
+        switch ((int)totalValue) {
+            case 100:
+                return totalValue * 0.99;
+            case 200:
+                return totalValue * 1.99 ;
+            default:
+                return totalValue;
+        }
+    }
+
+    public void showList(){
+        System.out.println("LISTA");
+        int i = 0;
+        while (i < list.length) {
+            System.out.println(list[i]);
+            i++;
+        }
+        System.out.println("\nLISTA");
+        i = 0;
+        do {
+            System.out.println(list[i]);
+            i++;
+        } while (i < list.length);
+
+        System.out.println("\nLISTA");
+        for (int j = 0; j < list.length; j++) {
+            System.out.println(list[j]);
+        }
+
+        System.out.println("\nLISTA");
+        for(String item : list) {
+            System.out.println(item);
+        }
     }
 
     @Override
