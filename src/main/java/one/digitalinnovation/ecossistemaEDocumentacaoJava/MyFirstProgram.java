8f3254e690be1fd49e1baf595fd0fa9b54986d0d
Felipe-builder <felipesvascon@gmail.com>
2022-01-22 10:26:01 -0300

    aulas de ecossistema de java

--- src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/MyFirstProgram.java
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/MyFirstProgram.java
@@ -4,6 +4,7 @@ import one.digitalinnovation.ecossistemaEDocumentacaoJava.base.Order;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        //testes
         Order order = new Order("e1020eok", 250.42, new String[]{"arroz", "feijão", "carne"});
         System.out.println(order);
         System.out.println(order.caculatedIf());
