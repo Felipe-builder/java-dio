0f9ce29f314e7c47c64ba947aba49229ef7935ab
Felipe-builder <felipesvascon@gmail.com>
2022-01-24 22:59:13 -0300

    Aula sobre métodos

---
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/entendendo_metodos/criacao/Emprestimo.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.ecossistemaEDocumentacaoJava.entendendo_metodos.criacao;
+
+public class Emprestimo {
+    public static double receberComJuros(double valor) {
+        double juros = 0;
+        if (valor <= 1000) {
+            juros = 2.0;
+        } else if (valor <= 1500) {
+            juros = 2.5;
+        } else if (valor <= 2000) {
+            juros = 3;
+        } else if (valor <= 5000) {
+            juros = 3.5;
+        } else if(valor <= 10000) {
+            juros = 4;
+        } else if (valor > 10000) {
+            juros = 15;
+        }
+        return valor += valor * (juros / 100) ;
+    }
+}
---
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/entendendo_metodos/criacao/Exercicio3.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.ecossistemaEDocumentacaoJava.entendendo_metodos.criacao;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        System.out.println(Emprestimo.receberComJuros(10000));
+    }
+}
