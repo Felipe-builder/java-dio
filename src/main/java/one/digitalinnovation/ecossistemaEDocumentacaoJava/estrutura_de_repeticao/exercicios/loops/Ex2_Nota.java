47de2c100e258087ab3109072b9a46100be7cc30
Felipe-builder <felipesvascon@gmail.com>
2022-01-26 22:50:06 -0300

    Exercicios de Loops

---
+++ src/main/java/one/digitalinnovation/ecossistemaEDocumentacaoJava/estrutura_de_repeticao/exercicios/loops/Ex2_Nota.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.ecossistemaEDocumentacaoJava.estrutura_de_repeticao.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double nota;
+        System.out.print("Digite uma nota entre 0 e 10: ");
+        nota = scanner.nextDouble();
+
+        while (!(nota >= 0 && nota <= 10)) {
+            System.out.print("Valor inválido! digite novamente: ");
+            nota = scanner.nextDouble();
+        }
+        System.out.println("SUA NOTA FOI " + nota);
+    }
+}
