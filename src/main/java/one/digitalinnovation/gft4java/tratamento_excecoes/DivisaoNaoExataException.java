7e21b8a97637ffd01967f34e5709e0fbf4406eb4
Felipe-builder <felipesvascon@gmail.com>
2022-02-08 23:01:06 -0300

    Excecoes Personalizadas

---
+++ src/main/java/one/digitalinnovation/gft4java/tratamento_excecoes/DivisaoNaoExataException.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.gft4java.tratamento_excecoes;
+
+public class DivisaoNaoExataException extends Exception {
+    private int numerador;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+}
