7a04138014fa22b656c317c6787276622bd73c8c
Felipe-builder <felipesvascon@gmail.com>
2022-02-19 16:34:17 -0300

    Exercicio

---
+++ src/main/java/one/digitalinnovation/gft4java/Collections/sets/exercicios/ArcoIris.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gft4java.Collections.sets.exercicios;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ArcoIris {
+    public static void main(String[] args) {
+        Set<String> cores = new HashSet<>(Arrays.asList("Vermelho", "Azul", "Amarelo"));
+
+        System.out.println(cores);
+    }
+}
